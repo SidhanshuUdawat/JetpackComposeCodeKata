0a4ec12f0e99c9d4a5a2de2f477deebd9e65219b
Sidhanshu Udawat <sidhanshu.udawat@autoscout24.com>
2022-02-22 00:29:59 +0100

    Basic compose

--- app/src/main/java/com/app/composecodekata/basic/MainActivity.kt
+++ app/src/main/java/com/app/composecodekata/basic/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.app.composecodekata.basic
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
@@ -19,7 +21,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -27,6 +28,23 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.app.composecodekata.R
 import com.app.composecodekata.ui.theme.JetpackComposeCodeKataAS24Theme
+
+
+/**
+ * State in an app is any value that can change over time.
+ * This is a very broad definition and encompasses everything from a Room database to a variable on a class.
+ *
+ * Compose is declarative and as such the only way to update it is by calling the same composable with
+ * new arguments.
+ *
+ * These arguments are representations of the UI state.
+ * Any time a state is updated a recomposition takes place.
+ * As a result, things like TextField don’t automatically update like they do in imperative XML based views.
+ * A composable has to explicitly be told the new state in order for it to update accordingly.
+ *
+ * Ref: https://developer.android.com/jetpack/compose/state
+ * Imp: https://developer.android.com/jetpack/compose/mental-model
+ */
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,41 +57,50 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// Problem 1: On boarding screen will never go away let's try to find a solution to fix it
+// Hint: https://developer.android.com/jetpack/compose/state#state-in-composables
+
+// Problem 2: Composables with internal state tend to be less reusable and harder to test.
+// Hint 1: A stateless composable is a composable that doesn't hold any state. An easy way to achieve stateless is by using state hoisting.
+// https://developer.android.com/jetpack/compose/state#state-hoisting
+
+// Hint 2: create a separate composable function for on boarding screen which takes lambda as input
+
+// Problem 3: On boarding screen shows again on the configuration change for example on screen rotation.
+// How do we stop that?
+
+
 @Composable
 fun MyApp() {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    var shouldShowOnboarding = true
 
     if (shouldShowOnboarding) {
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        Surface {
+            Column(
+                modifier = Modifier.fillMaxSize()
+                // Let's try to align the content in the Center
+            ) {
+                Text("Welcome to the Basics Codelab!")
+                Button(
+                    modifier = Modifier.padding(vertical = 24.dp),
+                    onClick = { shouldShowOnboarding = false }
+                ) {
+                    Text("Continue")
+                }
+            }
+        }
     } else {
         Greetings()
     }
 }
 
+// Problem - 1: Let's increase the List size to 1k and see what happens?
+// Problem - 2: Use a replacement of RecyclerView with 3 lines of code, instead of multiple files ;)
 @Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
-    Surface {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Welcome to the Basics Codelab!")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = onContinueClicked
-            ) {
-                Text("Continue")
-            }
-        }
-    }
-}
-
-@Composable
-private fun Greetings(names: List<String> = List(1000) { "$it" }) {
-    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-        items(items = names) { name ->
-            Greeting(name = name)
+private fun Greetings(names: List<String> = List(2) { "$it" }) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        names.forEach {
+            Greeting(name = it)
         }
     }
 }
@@ -88,9 +115,14 @@ fun Greeting(name: String) {
     }
 }
 
+
+// Problem - 1: The expanded state is not behaving as expected let's find a solution
+// Problem - 2: View isn't the whole width of the screen what modifier would you use?
+// Problem - 3: Let's try to change the fontWeight for "MaterialTheme.typography.h4"
+
 @Composable
 private fun CardContent(name: String) {
-    var expanded by rememberSaveable { mutableStateOf(false) }
+    var expanded = false // Change here what you did earlier ;)
 
     Row(
         modifier = Modifier
@@ -104,15 +136,12 @@ private fun CardContent(name: String) {
     ) {
         Column(
             modifier = Modifier
-                .weight(1f)
                 .padding(12.dp)
         ) {
             Text(text = "Hello, ")
             Text(
                 text = name,
-                style = MaterialTheme.typography.h4.copy(
-                    fontWeight = FontWeight.ExtraBold
-                )
+                style = MaterialTheme.typography.h4
             )
             if (expanded) {
                 Text(
@@ -135,21 +164,17 @@ private fun CardContent(name: String) {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Preview
 @Composable
-fun OnboardingPreview() {
+fun OnboadingPreview() {
     JetpackComposeCodeKataAS24Theme {
-        OnboardingScreen(onContinueClicked = {}) // Do nothing on click.
+        MyApp()
     }
 }
 
-@Preview(
-    showBackground = true,
-    widthDp = 320,
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "DefaultPreviewDark"
-)
-@Preview(showBackground = true, widthDp = 320)
+// Make use of another @Preview tag to display the night mode version of the layout
+// Set width to increase the size
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeCodeKataAS24Theme {
--- app/src/main/java/com/app/composecodekata/ui/theme/Theme.kt
+++ app/src/main/java/com/app/composecodekata/ui/theme/Theme.kt
@@ -1,12 +1,12 @@
 package com.app.composecodekata.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
+// Try to use the DarkColor Palette to support dark theme
 private val DarkColorPalette = darkColors(
     surface = Blue,
     onSurface = Navy,
@@ -22,15 +22,11 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun JetpackComposeCodeKataAS24Theme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun JetpackComposeCodeKataAS24Theme(content: @Composable() () -> Unit) {
+    // Hint: a simple conditional check can be used to detect if the system is in dark theme
 
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
