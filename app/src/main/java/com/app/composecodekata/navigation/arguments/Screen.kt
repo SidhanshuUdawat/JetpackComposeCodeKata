4d4e246bdc60a91249c051e5ab638ef5355c54d5
Sidhanshu Udawat <sidhanshu.udawat@autoscout24.com>
2022-02-22 01:40:45 +0100

    Args nav problem

--- app/src/main/java/com/app/composecodekata/navigation/arguments/Screen.kt
+++ app/src/main/java/com/app/composecodekata/navigation/arguments/Screen.kt
@@ -1,5 +1,8 @@
 package com.app.composecodekata.navigation.arguments
 
+/**
+ * Problem: Detail Screen takes arguments and we need to extend the Detail object to make it more concrete
+ */
 sealed class Screen(val route: String) {
     object Home : Screen(route = "home_screen")
     object Detail : Screen(route = "detail_screen/id={id}?name={name}")
